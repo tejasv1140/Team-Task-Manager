340eabe0377c69323e5186875c2ea60b2ce5c8c1
Tejop Parashar <tejoparashar@gmail.com>
2026-05-02 21:55:21 +0530

    Validate production MongoDB URL

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
+const isLocalMongoUri = (uri) => /mongodb(\+srv)?:\/\/(localhost|127\.0\.0\.1|\[?::1\]?)/i.test(uri);
+
 const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGODB_URI || process.env.MONGO_URL || process.env.MONGODB_URL;
+    const mongoUris = [
+      process.env.MONGO_URL,
+      process.env.MONGODB_URI,
+      process.env.MONGODB_URL
+    ].filter(Boolean);
+
+    const mongoUri = process.env.NODE_ENV === 'production'
+      ? mongoUris.find(uri => !isLocalMongoUri(uri))
+      : mongoUris[0];
 
     if (!mongoUri) {
-      throw new Error('Missing MongoDB connection string. Set MONGODB_URI or MONGO_URL in Railway variables.');
+      throw new Error('Missing production MongoDB connection string. In Railway, set MONGODB_URI to ${{Mongo.MONGO_URL}} or remove any localhost Mongo URL.');
+    }
+
+    if (process.env.NODE_ENV === 'production' && isLocalMongoUri(mongoUri)) {
+      throw new Error('MongoDB URL points to localhost. In Railway, set MONGODB_URI to ${{Mongo.MONGO_URL}} instead.');
     }
 
     const conn = await mongoose.connect(mongoUri, {
